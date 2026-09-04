92f89d01c03b278458af0a14260f502ea7c65831
JuanJoseIA-UdC <juan.ipia53924@ucaldas.edu.co>
2026-09-04 13:00:26 -0500

    feat: agrega estructura inicial de trarjeta y estilos CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,30 @@
-console.log("Hola, desde JS");
-console.log('Hola, desde Js');
+// console.log("Hola, desde JS");
+// console.log('Hola, desde Js');
 
 
-/* Estructura de funciones */
-function saludar(){
-    console.log(`Hola, desde JS`);    
+// /* Estructura de funciones */
+// function saludar(){
+//     console.log(`Hola, desde JS`);    
+// }
+
+// const SALUDO_ARROW = () => {
+//     console.log('Hola, desde Js');
+// }
+
+// const SALUDO_ANONIMO = function(){
+//     const nombre_persona = "Jhoe Doe"
+//     console.log(`Hola, ${nombre_persona}`);
+//     console.log('Hola, ', nombre_persona);
+// }
+
+// /* Ejecucion de las funciones */
+
+// saludar();
+// SALUDO_ARROW();
+// SALUDO_ANONIMO();
+
+const BASE_PATH_POKEMON_API = 'https://pokeapi.co/api/v2/pokemon'
+
+const GET_POKEMON = () => {
+    
 }
-
-const SALUDO_ARROW = () => {
-    console.log('Hola, desde Js');
-}
-
-const SALUDO_ANONIMO = function(){
-    const nombre_persona = "Jhoe Doe"
-    console.log(`Hola, ${nombre_persona}`);
-}
-
-/* Ejecucion de las funciones */
-
-saludar();
-SALUDO_ARROW();
-SALUDO_ANONIMO();
-
