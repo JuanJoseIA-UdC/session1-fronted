35c60c27fbccb332f3c0ccf267b25531ba62d8cb
JuanJoseIA-UdC <juan.ipia53924@ucaldas.edu.co>
2026-09-04 07:55:51 -0500

    feat: Nwe Js file

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+console.log("Hola, desde JS");
+console.log('Hola, desde Js');
+
+
+/* Estructura de funciones */
+function saludar(){
+    console.log(`Hola, desde JS`);    
+}
+
+const SALUDO_ARROW = () => {
+    console.log('Hola, desde Js');
+}
+
+const SALUDO_ANONIMO = function(){
+    const nombre_persona = "Jhoe Doe"
+    console.log(`Hola, ${nombre_persona}`);
+}
+
+/* Ejecucion de las funciones */
+
+saludar();
+SALUDO_ARROW();
+SALUDO_ANONIMO();
+
