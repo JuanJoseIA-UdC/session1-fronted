e799faedaaa98b970cb44ae08d4e68cfdfae0bfa
JuanJoseIA-UdC <juan.ipia53924@ucaldas.edu.co>
2026-09-10 16:45:29 -0500

    style: Practice testing CSS styles and starting to practice consuming APIs.

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,13 @@
 // console.log("Hola, desde JS");
 // console.log('Hola, desde Js');
 
-
 // /* Estructura de funciones */
 // function saludar(){
 //     console.log(`Hola, desde JS`);    
 // }
-
 // const SALUDO_ARROW = () => {
 //     console.log('Hola, desde Js');
 // }
-
 // const SALUDO_ANONIMO = function(){
 //     const nombre_persona = "Jhoe Doe"
 //     console.log(`Hola, ${nombre_persona}`);
@@ -18,13 +15,24 @@
 // }
 
 // /* Ejecucion de las funciones */
-
 // saludar();
 // SALUDO_ARROW();
 // SALUDO_ANONIMO();
 
+function Saludar(nombreP){
+    console.log(nombreP);
+    console.log("Hola", nombreP)
+    console.log("Hola", nombreP)
+    console.log(`Hola ${nombreP}`)
+
+}
+
 const BASE_PATH_POKEMON_API = 'https://pokeapi.co/api/v2/pokemon'
 
-const GET_POKEMON = () => {
-    
+const GET_POKEMONS = async()=> {
+    const response = await fetch(BASE_PATH_POKEMON_API);
+    console.log(response)
 }
+
+
+
