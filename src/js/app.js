7a950713e71da6df4ce2aa55cee9e82d564743ab
JuanJoseIA-UdC <juan.ipia53924@ucaldas.edu.co>
2026-09-11 09:15:00 -0500

    feat: Consumi la api

--- src/js/app.js
+++ src/js/app.js
@@ -19,19 +19,42 @@
 // SALUDO_ARROW();
 // SALUDO_ANONIMO();
 
-function Saludar(nombreP){
+/* function Saludar(nombreP){
     console.log(nombreP);
     console.log("Hola", nombreP)
     console.log("Hola", nombreP)
     console.log(`Hola ${nombreP}`)
 
-}
+} */
 
-const BASE_PATH_POKEMON_API = 'https://pokeapi.co/api/v2/pokemon'
+const BASE_PATH_POKEMON_API = "https://pokeapi.co/api/v2/pokemon";
 
-const GET_POKEMONS = async()=> {
-    const response = await fetch(BASE_PATH_POKEMON_API);
-    console.log(response)
+const GET_POKEMONS = async(cantidad)=> {
+    /* console.log(`${BASE_PATH_POKEMON_API}?limit=${cantidad}`) */
+    const response = await fetch(`${BASE_PATH_POKEMON_API}?limit=${cantidad}`);
+    const pokemons = await response.json();
+
+    /* console.log(pokemons); */
+    /* Convertir texto plano... */
+
+    /* Acceder a un elemento especifico del HTML */
+    const container = document.getElementById("pokemons-cards"); /* Forma tradicional (No es la forma en que la que ahora se maneja) */
+    
+    
+    const container2 = document.querySelector(".pokemons-cards");
+
+    
+
+    pokemons.results.forEach((pokemon) => {
+        console.log(pokemon);
+        container2.innerHTML += `<div class="pokemon-card">
+            <h3>${pokemon.name}</h3>
+            <img src="" alt="">
+            <button class="btn">Ver mas</button>
+            </img>
+        </div>`;
+    });
+    
 }
 
 
